bcf218edc47c1c929916a5e0b5a14227caf20229
Vitor Oliveira <vmoliveira91@gmail.com>
2021-09-18 12:44:54 -0300

    Adjusted use of theme on sidebar.tsx

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
-import { theme } from 'resources/theme'
 import { Header } from './header'
 import { ListArea } from './list_area'
 
 const SidebarStyle = styled.aside`
   display: grid;
   grid-template-rows: 1fr 5fr;
-  background-color: ${theme.colors.black};
+  background-color: ${({ theme }) => theme.colors.black};
   height: 100vh;
 `
 
