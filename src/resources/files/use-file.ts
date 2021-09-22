4d27386de0858aaafc241a7c87c4ef76a6a2be8c
Vitor Oliveira <vmoliveira91@gmail.com>
2021-09-22 20:44:06 -0300

    Added localforage on the initial loading

--- src/resources/files/use-file.ts
+++ src/resources/files/use-file.ts
@@ -1,9 +1,39 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { File } from 'helpers/types/file'
+import { v4 as uuidv4 } from 'uuid'
+import localforage from 'localforage'
 
 export const useFiles = () => {
   const inputRef = useRef<HTMLInputElement>(null)
   const [files, setFiles] = useState<File[]>([])
+
+  useEffect(() => {
+    const getFiles = async () => {
+      const files: File[] | null = await localforage.getItem('files')
+
+      console.log(files)
+
+      if (!files) {
+        inputRef.current?.focus()
+
+        const newFile: File = {
+          id: uuidv4(),
+          name: 'Sem título',
+          content: '',
+          active: true,
+          status: 'saved',
+        }
+
+        setFiles([newFile])
+
+        await localforage.setItem('files', [newFile])
+      } else {
+        setFiles(files)
+      }
+    }
+
+    getFiles()
+  }, [])
 
   useEffect(() => {
     let timeoutId: ReturnType<typeof setTimeout>
